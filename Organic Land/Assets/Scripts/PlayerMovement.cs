7dd3d319d2a698e6924991aae12615dc333744ef
Dominik07 <60546513+SPRINT4848@users.noreply.github.com>
2020-03-07 16:13:04 +0100

    Player movement is now relative to the camera

--- Organic Land/Assets/Scripts/PlayerMovement.cs
+++ Organic Land/Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float movementSpeed = 7f;
+    private Transform cameraTransform;
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+    }
 
     private void Update()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
 
-        transform.position += new Vector3(x, 0, z) * movementSpeed * Time.fixedDeltaTime;
+        Vector3 moveX = cameraTransform.right * x;
+        Vector3 moveZ = cameraTransform.forward * z;
+
+        Vector3 movement = moveX + moveZ;
+
+        transform.position += new Vector3(movement.x, 0, movement.z) * movementSpeed * Time.fixedDeltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Q))
+            Rotate(45f);
+        else if (Input.GetKeyDown(KeyCode.E))
+            Rotate(-45f);
+    }
+
+    void Rotate(float amount)
+    {
+        transform.Rotate(0, amount, 0, Space.World);
     }
 
 }
