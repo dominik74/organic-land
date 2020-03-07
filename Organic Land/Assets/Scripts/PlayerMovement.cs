bc9c97e7255fc36258c2b98f0c4a365b1a86383f
Dominik07 <60546513+SPRINT4848@users.noreply.github.com>
2020-03-07 11:57:32 +0100

    Implement basic player movement

---
+++ Organic Land/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float movementSpeed = 7f;
+
+    private void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        transform.position += new Vector3(x, 0, z) * movementSpeed * Time.fixedDeltaTime;
+    }
+
+}
