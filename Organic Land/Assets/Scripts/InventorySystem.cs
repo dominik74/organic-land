201e77260a4cc5b194cca83b1593bcede9d91f29
Dominik07 <60546513+SPRINT4848@users.noreply.github.com>
2020-03-07 12:33:14 +0100

    Inventory System pt. 1

---
+++ Organic Land/Assets/Scripts/InventorySystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour {
+
+    public Transform slotsParent;
+
+    public ItemData[] items;
+
+    public void AddItem(string name)
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            if(items[i].name == name)
+            {
+                // TODO: Implement
+                Debug.Log("Added item");
+            }
+        }
+    }
+
+}
