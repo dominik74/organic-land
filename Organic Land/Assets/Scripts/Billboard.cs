4f79986d0f6ae390d854f981c478b508901e6fa6
Dominik07 <60546513+SPRINT4848@users.noreply.github.com>
2020-03-07 14:48:38 +0100

    Create Billboard.cs

---
+++ Organic Land/Assets/Scripts/Billboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+    private Transform cam;
+
+    private void Start()
+    {
+        cam = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        transform.LookAt(cam);    
+    }
+
+}
