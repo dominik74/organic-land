140a916fb3f14c7f1e420e13d2ae293610262e93
Dominik07 <60546513+SPRINT4848@users.noreply.github.com>
2020-03-08 18:03:40 +0100

    Terrain Generator pt. 1

---
+++ Organic Land/Assets/Scripts/ObjectData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Object", menuName = "ObjectData")]
+public class ObjectData : ScriptableObject {
+
+    public Sprite sprite;
+    public int spawnChance = 100;
+    public Color color = Color.white;
+
+}
---
+++ Organic Land/Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour {
+
+    public float terrainSize;
+
+    [Range(1, 300)]
+    public int maxObjectCount;
+
+    [Space]
+    public GameObject objectTemplate;
+    public ObjectData[] objectData;
+
+    private void Start()
+    {
+        Generate();
+    }
+
+    void Generate()
+    {
+        int objectsSpawned = 0;
+
+        while(objectsSpawned < maxObjectCount)
+        {
+            float random = Random.Range(0, 100);
+
+            int lowLimit = 0;
+            int highLimit = 0;
+
+            for (int i = 0; i < objectData.Length; i++)
+            {
+                lowLimit = highLimit;
+                highLimit += objectData[i].spawnChance;
+
+                if (random >= lowLimit && random < highLimit)
+                {
+                    GameObject obj = Instantiate(objectTemplate);
+                    InitializeObject(obj, objectData[i]);
+                    obj.transform.position = new Vector3(Random.Range(-terrainSize, terrainSize), 1, Random.Range(-terrainSize, terrainSize));
+
+                    objectsSpawned++;
+                    break;
+                }
+
+            }
+        }
+
+        Debug.Log(objectsSpawned);
+
+    }
+
+    void InitializeObject(GameObject obj, ObjectData data)
+    {
+        obj.name = data.name;
+        obj.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = data.sprite;
+        obj.transform.GetChild(0).GetComponent<SpriteRenderer>().color = data.color;
+    }
+
+}
