1d68d6fe17fb77c8a277788ce90f4d0c3eebee67
Dominik07 <60546513+SPRINT4848@users.noreply.github.com>
2020-03-11 14:36:18 +0100

    Implement point and click movement

--- Organic Land/Assets/Scripts/PlayerMovement.cs
+++ Organic Land/Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float movementSpeed = 7f;
+    public float stopPointMinimumDistance = 0.2f;
     private Transform cameraTransform;
+    private Camera cam;
+
+    private bool isMoving;
 
     private void Start()
     {
         cameraTransform = Camera.main.transform;
+        cam = Camera.main;
     }
 
     private void Update()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
+
+        if (x != 0 || z != 0)
+            isMoving = false;
 
         Vector3 moveX = cameraTransform.right * x;
         Vector3 moveZ = cameraTransform.forward * z;
@@ -28,11 +36,44 @@ public class PlayerMovement : MonoBehaviour {
             Rotate(-45f);
         else if (Input.GetKeyDown(KeyCode.E))
             Rotate(45f);
+
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag("Ground"))
+                {
+                    isMoving = true;
+                    StopCoroutine("MoveTo");
+                    StartCoroutine("MoveTo", hit.point);
+                }
+            }
+        }
+
     }
 
     void Rotate(float amount)
     {
         transform.Rotate(0, amount, 0, Space.World);
+    }
+
+    IEnumerator MoveTo(Vector3 targetPos)
+    {
+        while (isMoving)
+        {
+            Vector3 dir = (targetPos - transform.position).normalized;
+            dir.y = 0;
+            transform.position += dir * movementSpeed * Time.fixedDeltaTime;
+
+            if ((targetPos - transform.position).sqrMagnitude <= stopPointMinimumDistance * stopPointMinimumDistance)
+                isMoving = false;
+
+            yield return null;
+        }
     }
 
 }
