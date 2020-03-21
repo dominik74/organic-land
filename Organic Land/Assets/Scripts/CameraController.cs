e15f0e527b21708c89bef95142e6c1622e4c8f0f
Dominik07 <60546513+SPRINT4848@users.noreply.github.com>
2020-03-21 14:45:43 +0100

    Implement camera zoom

--- Organic Land/Assets/Scripts/CameraController.cs
+++ Organic Land/Assets/Scripts/CameraController.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+    public Vector3 maxOffset;
+    public float maxRotation;
+
+    private Vector3 minOffset;
+    private float minRotation;
+    private float zoomFactor = 4f;
+
+    private float currentScroll;
+
     private CameraFollow cameraFollow;
 
     private void Start()
     {
         cameraFollow = GetComponent<CameraFollow>();
+        minOffset = cameraFollow.offset;
+        minRotation = transform.eulerAngles.x;
     }
 
     private void Update()
@@ -20,11 +31,21 @@ public class CameraController : MonoBehaviour {
             Rotate(-45f);
         else if (Input.GetKeyDown(KeyCode.E))
             Rotate(45f);
+
+        float scrollData = Input.GetAxis("Mouse ScrollWheel");
+
+        currentScroll -= scrollData;
+        currentScroll = Mathf.Clamp01(currentScroll);
+
+        cameraFollow.offset = Vector3.Lerp(minOffset, maxOffset, currentScroll);
+        transform.eulerAngles = Vector3.Lerp(new Vector3(minRotation, transform.eulerAngles.y, transform.eulerAngles.z), new Vector3(maxRotation, transform.eulerAngles.y, transform.eulerAngles.z), currentScroll);
     }
 
     void Rotate(float amount)
     {
         cameraFollow.offset = Quaternion.Euler(0, amount, 0) * cameraFollow.offset;
+        minOffset = Quaternion.Euler(0, amount, 0) * minOffset;
+        maxOffset = Quaternion.Euler(0, amount, 0) * maxOffset;
         transform.Rotate(0, amount, 0, Space.World);
     }
 
