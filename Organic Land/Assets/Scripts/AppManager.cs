f2082d65f98e3dfb5a1be331b1b9d39c6d750ca7
Dominik07 <60546513+SPRINT4848@users.noreply.github.com>
2020-03-25 21:34:48 +0100

    Add and apply AppManager.cs

---
+++ Organic Land/Assets/Scripts/AppManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour {
+
+    public void ToggleFullsreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+
+	public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quitting application...");
+    }
+
+}
