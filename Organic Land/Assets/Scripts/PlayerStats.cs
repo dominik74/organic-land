e98fb4125c9553797d557a1492b5cd28931d8d45
Dominik07 <60546513+SPRINT4848@users.noreply.github.com>
2020-03-26 17:19:33 +0100

    Player Stats pt. 2

--- Organic Land/Assets/Scripts/PlayerStats.cs
+++ Organic Land/Assets/Scripts/PlayerStats.cs
@@ -10,16 +10,42 @@ public class PlayerStats : MonoBehaviour {
 
     [Space]
     public float hungerDropRate = 0.25f;
+    public float healthDropRate = 0.75f;
 
     private float currentHealth;
     private float currentHunger;
     private float currentThirst;
+
+    public class PlayerStatsContainer
+    {
+        public float health;
+        public float hunger;
+        public float thirst;
+
+        public PlayerStatsContainer(float health, float hunger, float thirst)
+        {
+            this.health = health;
+            this.hunger = hunger;
+            this.thirst = thirst;
+        }
+    }
+
+    public static PlayerStats instance;
+    private void Awake()
+    {
+        instance = this;
+    }
 
     private void Start()
     {
         ResetStats();
 
         StartCoroutine("HungerTimer");
+    }
+
+    public PlayerStatsContainer GetCurrentStats()
+    {
+        return new PlayerStatsContainer(currentHealth, currentHunger, currentThirst);
     }
 
     void ResetStats()
@@ -29,14 +55,39 @@ public class PlayerStats : MonoBehaviour {
         currentThirst = maxThirst;
     }
 
+    void Die()
+    {
+        Debug.Log("You died!");
+    }
+
     IEnumerator HungerTimer()
     {
         while (currentHunger > 0)
         {
             currentHunger--;
+            EventManager.StatsUpdated();
             yield return new WaitForSeconds(hungerDropRate);
         }
         currentHunger = 0;
+
+        StopCoroutine("DamageTimer");
+        StartCoroutine("DamageTimer");
+    }
+
+    IEnumerator DamageTimer()
+    {
+        while (currentHunger <= 0)
+        {
+            currentHealth--;
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+                Die();
+                yield break;
+            }
+            EventManager.StatsUpdated();
+            yield return new WaitForSeconds(healthDropRate);
+        }
     }
 
 }
---
+++ Organic Land/Assets/Scripts/PlayerStatsTracker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatsTracker : MonoBehaviour {
+
+    public Slider healthbar, hungerbar, thirstbar;
+
+    private PlayerStats playerStats;
+
+    private void Start()
+    {
+        playerStats = PlayerStats.instance;
+        InitializeStatusBars();
+    }
+
+    private void OnEnable()
+    {
+        EventManager.OnStatsUpdated += UpdateStats;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnStatsUpdated -= UpdateStats;
+    }
+
+    void InitializeStatusBars()
+    {
+        healthbar.maxValue = playerStats.maxHealth;
+        hungerbar.maxValue = playerStats.maxHunger;
+        thirstbar.maxValue = playerStats.maxThirst;
+    }
+
+    void UpdateStats()
+    {
+        healthbar.value = playerStats.GetCurrentStats().health;
+        hungerbar.value = playerStats.GetCurrentStats().hunger;
+        thirstbar.value = playerStats.GetCurrentStats().thirst;
+    }
+
+}
