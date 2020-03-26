b710d089645035a0f7e287f0c09d4a748a85471f
Dominik07 <60546513+SPRINT4848@users.noreply.github.com>
2020-03-26 15:23:52 +0100

    Add PlayerStats.cs

---
+++ Organic Land/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    public float maxHealth = 100;
+    public float maxHunger = 150;
+    public float maxThirst = 125;
+
+    [Space]
+    public float hungerDropRate = 0.25f;
+
+    private float currentHealth;
+    private float currentHunger;
+    private float currentThirst;
+
+    private void Start()
+    {
+        ResetStats();
+
+        StartCoroutine("HungerTimer");
+    }
+
+    void ResetStats()
+    {
+        currentHealth = maxHealth;
+        currentHunger = maxHunger;
+        currentThirst = maxThirst;
+    }
+
+    IEnumerator HungerTimer()
+    {
+        while (currentHunger > 0)
+        {
+            currentHunger--;
+            yield return new WaitForSeconds(hungerDropRate);
+        }
+        currentHunger = 0;
+    }
+
+}
