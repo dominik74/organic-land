cd1f75210fa42dd432e156d503cf8cd6fb62bfd0
Dominik07 <60546513+SPRINT4848@users.noreply.github.com>
2020-03-26 13:21:16 +0100

    Tweak button select design

---
+++ Organic Land/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Button))]
+public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler {
+
+    private Color highlightColor;
+    private Color defaultColor;
+    private Color pressedColor;
+
+    private Text myText;
+
+    private void Start()
+    {
+        myText = transform.GetChild(0).GetComponent<Text>();
+        defaultColor = myText.color;
+        highlightColor = GetComponent<Button>().colors.highlightedColor;
+        pressedColor = GetComponent<Button>().colors.pressedColor;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        myText.color = highlightColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        myText.color = defaultColor;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        myText.color = pressedColor;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        myText.color = defaultColor;
+    }
+}
