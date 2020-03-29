181d0eb7bddc1c1005c9805d71558d3dda7f3155
Dominik07 <60546513+SPRINT4848@users.noreply.github.com>
2020-03-29 14:25:18 +0200

    Add right-click to use feature (outer hotbar slots)

--- Organic Land/Assets/Scripts/HotbarSlot.cs
+++ Organic Land/Assets/Scripts/HotbarSlot.cs
@@ -8,6 +8,12 @@ public class HotbarSlot : MonoBehaviour,  IPointerDownHandler
     public void OnPointerDown(PointerEventData eventData)
     {
         InventorySystem.instance.SelectSlot(transform);
+
+        if(transform.childCount != 0)
+        {
+            if (eventData.button == PointerEventData.InputButton.Right)
+                InventorySystem.instance.UseSelectedItem();
+        }
     }
 
 }
