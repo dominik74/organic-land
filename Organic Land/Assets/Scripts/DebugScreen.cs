e071b183e46264e8d23b6822813bf200a7af7a18
Dominik07 <60546513+SPRINT4848@users.noreply.github.com>
2020-05-03 22:54:28 +0200

    Implement debug screen

---
+++ Organic Land/Assets/Scripts/DebugScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugScreen : MonoBehaviour {
+	
+	public float refreshRate = 0.5f;
+	
+	private Text debugTxt;
+	private float timer;
+	
+	void Start()
+	{
+		debugTxt = GetComponent<Text>();
+	}
+	
+	void Update()
+	{
+		if (Time.unscaledTime > timer)
+		{
+			Refresh();
+			timer = Time.unscaledTime + refreshRate;
+		}
+	}
+	
+	void Refresh()
+	{
+		Vector3 playerPos = PlayerManager.playerUnit.transform.position;
+		
+		debugTxt.text = string.Format("X: {0} / Z: {1}", Mathf.RoundToInt(playerPos.x), Mathf.RoundToInt(playerPos.z));
+	}
+	
+}
