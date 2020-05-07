f2ab735338c9f28302d1279a9b1a7e6c0ea176a7
Dominik07 <60546513+SPRINT4848@users.noreply.github.com>
2020-05-07 21:28:04 +0200

    Change default view distance

--- Organic Land/Assets/Scripts/Settings.cs
+++ Organic Land/Assets/Scripts/Settings.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Settings {
 
 	public static readonly int[] renderDistanceValues = { 3, 5, 7, 10 };
-	public static int renderDistance = renderDistanceValues[1];
+	public static int renderDistance = renderDistanceValues[2];
 
 	public static bool smoothRotation = true;
 
